30f1f4993e357032e360f0169c287332041fdefe
Nejat Boy <nejatboy34@gmail.com>
2025-05-31 19:49:53 +0300

    Development.

--- app/src/main/java/com/example/programmatictraining/base/BaseLayout.kt
+++ app/src/main/java/com/example/programmatictraining/base/BaseLayout.kt
@@ -1,6 +1,8 @@
 package com.example.programmatictraining.base
 
 import android.content.Context
+import android.content.res.Resources
+import android.util.TypedValue
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.constraintlayout.widget.ConstraintSet
 
@@ -9,4 +11,8 @@ import androidx.constraintlayout.widget.ConstraintSet
 abstract class BaseLayout(context: Context): ConstraintLayout(context) {
 
     protected val set = ConstraintSet()
+
+    protected val Int.dp: Int get() {
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, this.toFloat(), Resources.getSystem().displayMetrics).toInt()
+    }
 }
--- app/src/main/java/com/example/programmatictraining/ui/spash/SplashLayout.kt
+++ app/src/main/java/com/example/programmatictraining/ui/spash/SplashLayout.kt
@@ -2,28 +2,29 @@ package com.example.programmatictraining.ui.spash
 
 import android.content.Context
 import android.graphics.Typeface
-import androidx.appcompat.widget.AppCompatTextView
+import android.util.TypedValue
+import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintSet
 import com.example.programmatictraining.base.BaseLayout
 
 
-
 class SplashLayout(context: Context) : BaseLayout(context) {
 
-    val textView = AppCompatTextView(context)
+    val textView = TextView(context)
 
 
     init {
         textView.id = generateViewId()
         textView.layoutParams = LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT)
         textView.typeface = Typeface.DEFAULT_BOLD
-        textView.textSize = 30f
+        textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20f)
 
         addView(textView)
 
         set.clone(this)
+        set.connect(textView.id, ConstraintSet.START, ConstraintSet.PARENT_ID, ConstraintSet.START, 20.dp)
+        set.connect(textView.id, ConstraintSet.END, ConstraintSet.PARENT_ID, ConstraintSet.END, 20.dp)
         set.centerVertically(textView.id, ConstraintSet.PARENT_ID)
-        set.centerHorizontally(textView.id, ConstraintSet.PARENT_ID)
         set.applyTo(this)
     }
 }
